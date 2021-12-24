2feeb0137b769a64b85e8eca2b5d1c3f474d8639
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-12-24 16:38:08 -0500

    Useless commit

--- launch-wf.js
+++ launch-wf.js
@@ -1,4 +1,5 @@
 var currentURL = window.location.href;
 let result = currentURL.replace("https://www.workflow.is/workflows/", "shortcuts://workflows/");
+result = result.replace("http://www.workflow.is/workflows/", "shortcuts://workflows/");
 result = result.replace(".html", "");
 window.open(result, '_blank');
